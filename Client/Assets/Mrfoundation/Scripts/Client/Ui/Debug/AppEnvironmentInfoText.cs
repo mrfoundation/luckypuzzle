4c22c022bcbc6b18ac8a44bfa07165ab2f7c0264
oojjrs <oojjrs@gmail.com>
2025-09-23 10:42:04 +0900

    앱 개발 및 실행 환경을 좀 더 유용하게 구조화

---
+++ Client/Assets/Mrfoundation/Scripts/Client/Ui/Debug/AppEnvironmentInfoText.cs
@@ -0,0 +1,14 @@
+﻿using Assets.Mrfoundation.Scripts.Client.Control.Primitive;
+using UnityEngine;
+
+namespace Assets.Mrfoundation.Scripts.Client.Ui.Debug
+{
+    [RequireComponent(typeof(MyTextForTmp))]
+    public class AppEnvironmentInfoText : MonoBehaviour
+    {
+        private void Start()
+        {
+            GetComponent<MyTextForTmp>().Text = MyEnv.AppEnvironmentInfo;
+        }
+    }
+}
